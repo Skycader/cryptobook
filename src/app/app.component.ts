2891cd6acc5fee53efbb0a90624fa2809144fdfa
skycader <skycader@yandex.ru>
2024-09-14 15:55:35 +0300

    main: first v

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import * as CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'cryptobook';
+  value = '';
+  password = '';
+  constructor() {}
+
+  encrypt(value: string): string {
+    return CryptoJS.AES.encrypt(value, this.password.trim()).toString();
+  }
+
+  decrypt(textToDecrypt: string) {
+    let a = CryptoJS.AES.decrypt(textToDecrypt, this.password.trim());
+    console.log(a);
+    return a.toString(CryptoJS.enc.Utf8);
+  }
 }
